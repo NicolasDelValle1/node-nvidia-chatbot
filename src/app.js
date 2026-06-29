d11c6e7f5a182ffde19d933219dead194be7c187
NicolasDelValle1 <nicolasdelvalle2006@gmail.com>
2026-06-29 17:18:52 -0500

    prueba app.js

--- src/app.js
+++ src/app.js
@@ -18,13 +18,13 @@ app.use(cors({
 app.use(express.json());
 
 app.use(express.static(
-    path.join(__dirname, "../public")
+    path.join(__dirname, "../frontend")
 ));
 
 app.get("/", (req,res)=>{
 
     res.sendFile(
-        path.join(__dirname, "../public/index.html")
+        path.join(__dirname, "../frontend/index.html")
     );
 
 });
